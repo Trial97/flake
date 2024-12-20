781e112e9c97f6766e14d3f74ab343cb1cfe291f
Trial97 <alexandru.tripon97@gmail.com>
2024-12-20 15:19:31 +0200

    Add dev shell
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake nixos configuration";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+        ./dev.nix
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+    };
+}
